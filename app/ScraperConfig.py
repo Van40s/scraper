841b1863dc1d9f3c92a13de214245b02c5bbb520
Van40s <vanestojanov49@gmail.com>
2025-02-15 19:08:47 +0100

    cleaning tests

--- app/ScraperConfig.py
+++ app/ScraperConfig.py
@@ -30,8 +30,6 @@ class ScraperConfig:
 
         username = os.getenv("PROXY_USERNAME", None)
         password = os.getenv("PROXY_PASSWORD", None)
-        print(username)
-        print(password)
         if username and password:
             self.proxy = f"http://{username}:{password}@dc.smartproxy.com:10000"
         else:
@@ -39,13 +37,6 @@ class ScraperConfig:
 
 
     def scrape_page(self, facebook_page_url):
-        url = 'https://ip.smartproxy.com/json'
-
-        result = requests.get(url, proxies={
-            'http': self.proxy,
-            'https': self.proxy
-        })
-        print(result.text)
         response = requests.get(facebook_page_url, proxies={
             'http': self.proxy,
             'https': self.proxy
@@ -81,7 +72,3 @@ class ScraperConfig:
                 return {}
 
         return gold_data
-
-if __name__ == "__main__":
-    sraper = ScraperConfig()
-    sraper.scrape_page("https://www.facebook.com/ApasievLevica/")
